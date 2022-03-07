303e2cfb9d07cbeed243de2d1f534ca6714cf7d8
Tanimara Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2022-03-07 13:36:48 +1100

    Update Joke.js
    Practicing ternary conditionals

--- src/Joke.js
+++ src/Joke.js
@@ -10,8 +10,11 @@ export default function Joke(props) {
     <div>
       {props.setup && <h3>{props.setup}</h3>}
       {isShown && <p>{props.punchline}</p>}
-      {isShown && <button onClick={toggleShown}>Hide punchline</button>}
-      {isShown && <button onClick={toggleShown}>Show punchline</button>}
+
+      <button onClick={toggleShown}>
+        {isShown ? "Hide" : "Show"} punchline
+      </button>
+
       <hr />
     </div>
   );
